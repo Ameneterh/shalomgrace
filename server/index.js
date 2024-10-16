04c422de42cb7a80733e21386ae1185bd6b5a306
amspharmacy <ameneterh@gmail.com>
2024-10-16 11:36:54 +0100

    change folder structure & names

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 // app.use((err, req, res, next) => {
