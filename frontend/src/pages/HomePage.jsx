12bfce107e17807d5caf7ce65e62ebdfc1ed7e7c
amspharmacy <ameneterh@gmail.com>
2024-10-16 14:01:05 +0100

    complete hero section of homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import heroimage from "/hero_img.png";
 
 export default function HomePage() {
@@ -10,9 +11,23 @@ export default function HomePage() {
         // backgroundSize: "",
         backgroundRepeat: "no-repeat",
       }}
-      className="min-h-screen w-full"
+      className="min-h-screen w-full flex justify-start"
     >
-      HomePage
+      <div className="text-white text-xl bg-[#D75825] px-4 sm:px-20 w-full bg-opacity-70 py-20 sm:py-40 flex flex-col gap-4">
+        <p>We provide</p>
+        <h1 className="text-5xl sm:text-7xl font-extrabold">
+          Innovative <br />
+          Healthcare <br />
+          Solutions
+        </h1>
+
+        <Link
+          to={"/services"}
+          className="px-4 py-3 bg-white text-[#D75825] text-sm w-96 text-center rounded-lg hover:bg-[#D75825] hover:text-white"
+        >
+          View Solutions
+        </Link>
+      </div>
     </div>
   );
 }
